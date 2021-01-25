287f0b004b711f31d062e64ffcc4de95c2b386ea
unknown <qamar.abbas14@hotmail.com>
2021-01-26 00:17:44 +0500

    Initial Commit --Test Methods left

--- main.py
+++ main.py
@@ -16,27 +16,31 @@ app=Flask(__name__)
 
 @app.route('/ProcessPayment/', methods=['GET'])
 def ProcessPayment():
-    req_data=request.get_data()
-    if req_data:
-        data=json.loads(req_data)
+    if request.method == 'GET':
+        req_data=request.get_data()
+        if req_data:
+            data=json.loads(req_data)
+        else:
+            return "<h2>The request is invalid: 400 bad request</h2>", 400
+
+        print(data)
+        card=Card_Verifications()
+        validity=card.verify(data)
+        print("Complete Validity is :",validity)
+
+        if validity:
+            print("Proceeding for payment methods")
+            amount=data['AMT']
+            obj=MakePayment(data)
+            msg, code =obj.select_payment_gateway()
+            print(msg,code)
+            return msg, code
+
+        else:
+            return "<h2>The request is invalid: 400 bad request</h2>", 400
     else:
         return "<h2>The request is invalid: 400 bad request</h2>", 400
 
-    print(data)
-    card=Card_Verifications()
-    validity=card.verify(data)
-    print("Complete Validity is :",validity)
-
-    if validity:
-        print("Proceeding for payment methods")
-        amount=data['AMT']
-        obj=MakePayment(data)
-        msg, code =obj.select_payment_gateway()
-        print(msg,code)
-        return msg, code
-
-    else:
-        return "<h2>The request is invalid: 400 bad request</h2>", 400
 
 
 if __name__=='__main__':
